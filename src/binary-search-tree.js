6b2f3c197681fc1f3c30e7694b3d2d59764413ea
koeebeth <96339397+koeebeth@users.noreply.github.com>
2023-12-16 14:09:04 +0300

    complete task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,45 +1,143 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
+    return this.rootNode;
+  }
+
+  add(data) {
+    if (!this.rootNode) {
+      this.rootNode = new Node(data);
+    }
+    addToTree(this.rootNode, data);
+
+    function addToTree(node, value) {
+      if (node === null) {
+        return new Node(value)
+      }
+      else if (node.data > value) {
+        node.left = addToTree(node.left, value);
+      }
+      else if(node.data < value) {
+        node.right = addToTree(node.right, value);
+      }
+      else {
+        return node;
+      }
+      return node;
+    }
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    return hasValue(this.rootNode, data);
+
+    function hasValue(node, value) {
+      if (node === null) {
+        return false;
+      }
+      else if (node.data === value) {
+        return true;
+      }
+      else if (node.data > value) {
+        return hasValue(node.left, value);
+      }
+      else {
+        return hasValue(node.right, value);
+      }
+    }
     // remove line with error and write your code here
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    return findValue(this.rootNode, data);
+
+    function findValue(node, value) {
+      if (node === null) {
+        return null;
+      }
+      else if (node.data === value) {
+        return node;
+      }
+      else if (node.data > value) {
+        return findValue(node.left, value);
+      }
+      else {
+        return findValue(node.right, value)
+      }
+    }
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    return removeFromTree(this.rootNode, data);
+    function removeFromTree(node, value) {
+      if(node === null) {
+        return null;
+      }
+      else if (node.data > value) {
+        node.left = removeFromTree(node.left, value);
+        return node;
+      }
+      else if (node.data < value) {
+        node.right = removeFromTree(node.right, value);
+        return node;
+      }
+      else {
+        if (node.left === null && node.right === null) {
+          return null;
+        }
+        else if (node.left === null) {
+          return node.right;
+        }
+        else if (node.right === null) {
+          return node.left;
+        }
+        else {
+          let maxLeft = node.left;
+          while (maxLeft.right !== null) {
+            maxLeft = maxLeft.right;
+          }
+          node.data = maxLeft.data;
+          node.left = removeFromTree(node.left, maxLeft.data);
+          return node;
+        }
+      }
+    }
     // remove line with error and write your code here
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
+    let ptr = this.rootNode;
+    if (ptr === null) {
+      return null;
+    }
+    while (ptr.left) {
+      ptr = ptr.left;
+    }
+    return ptr.data;
     // remove line with error and write your code here
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
+    let ptr = this.rootNode;
+    if (ptr === null) {
+      return null;
+    }
+    while (ptr.right !== null) {
+      ptr = ptr.right;
+    }
+    return ptr.data;
     // remove line with error and write your code here
   }
 }
